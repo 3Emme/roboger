dbad72ff18b140effa319d0da8f02dc431618e6c
3Emme <emmettbuentiempo@gmail.com>
2020-08-21 15:22:16 -0700

    troubleshoot triggerDetect function, and correct variable numberArray so it is an array of strings instead of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // Business Logic 
-function roboger (initialNumber){ 
-  console.log("initialNumber: " + initialNumber);
-  const numberArray =  countUp(initialNumber);
+function roboger (initialNumberString){ 
+  console.log("initialNumberString: " + initialNumberString);
+  const numberArray =  countUp(initialNumberString);
 
   function countUp(endNumber){
     let countArray = [];
     for (let i = 0; i <= parseInt(endNumber); i++){
-      countArray.push(i);
+      countArray.push(String(i));
     }
     return countArray;
   }
 
-  let arrayLength = parseInt(initialNumber) + 1;
+  let arrayLength = parseInt(initialNumberString) + 1;
   console.log("arrayLength: " + arrayLength);
   console.log("numberArray: " + numberArray);
   return triggerDetect(arrayLength,numberArray);
@@ -21,15 +21,20 @@ function triggerDetect(arrayLength,numberArray) {
   let triggerList = ["1","2","3"]; 
   let triggerOutput = [];
 
-  for (let index = 0; index < arrayLength; index +=1) {
-    let numberLength = numberArray[index].length;
-    let numberUnderScope = numberArray[index];
-    console.log("numberUnderScope: " + numberUnderScope);
+  //for (let index = 0; index < arrayLength; index +=1) {
+    for (let number of numberArray){
+    let numberLength = number.length;
+    let numberDigitArray = number.split("");
+    //let numberUnderScope = numberArray[index];
+    console.log("numberUnderScope: " + number);
+    console.log("Length of number under scope: " + numberLength);
     //const digitArray = numberUnderScope.split("");
-    console.log("numberArray index loop: " + index);
+    console.log("numberArray index looped");
     //console.log("digitArray: " + digitArray);
-    for (let index = 0; index < numberLength; index +=1){
-      console.log("numberArray digit loop: " + index);
+
+    //for (let index = 0; index < numberLength; index +=1){
+      for (let digit of number);
+      console.log("numberArray digit looped " + index);
       if (triggerList.includes(digitArray[index])) {
         // let digitArray = numberArray[index].split("");
         // let xArray = [];
@@ -48,8 +53,21 @@ function triggerDetect(arrayLength,numberArray) {
 
   }
 
-  return triggerOutput;
-} 
+  //return triggerOutput;
+//} 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
