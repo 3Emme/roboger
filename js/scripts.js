052cae230fcc9757aafad28e93050317fdedec1c
3Emme <emmettbuentiempo@gmail.com>
2020-08-23 23:53:20 -0700

    remove code that ended up not being used (array length, etc) and adjusted the strings being pushed to triggerOutput to give a better spacing effect.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,37 @@
 // Business Logic 
 function roboger (initialNumberString){ 
   const numberArray =  countUp(initialNumberString);
-
-  function countUp(endNumber){
-    let countArray = [];
-    for (let i = 0; i <= parseInt(endNumber); i++){
-      countArray.push(String(i));
-    }
-    return countArray;
-  }
-
-  let arrayLength = parseInt(initialNumberString) + 1;
-  return triggerDetect(arrayLength,numberArray);
+  return triggerDetect(numberArray);
 }
 
-function triggerDetect(arrayLength,numberArray) {  
+function countUp(endNumber){
+  let countArray = [];
+  for (let i = 0; i <= parseInt(endNumber); i++){
+    countArray.push(String(i));
+  }
+  return countArray;
+}
+
+function triggerDetect(numberArray) {  
   let triggerList = ["1","2","3"]; 
   let triggerOutput = [];
 
   for (let number of numberArray){
-    let numberLength = number.length;
     let numberDigitArray = number.split("");
     let triggerDigit = 0;
       for (let digit of numberDigitArray){
         if (triggerList.includes(digit) && parseInt(digit) > triggerDigit) {
           triggerDigit = digit;
-        } else {
-          }
+        } 
       }
     if (triggerDigit === "3"){
-      triggerOutput.push("Won't you be my neighbor?");
+      triggerOutput.push(" Won't you be my neighbor?");
     } else if (triggerDigit === "2"){
-      triggerOutput.push("Boop!");
+      triggerOutput.push(" Boop!");
     } else if (triggerDigit === "1"){
-      triggerOutput.push("Beep!");
+      triggerOutput.push(" Beep!");
     } else {
-      triggerOutput.push(number);
+      triggerOutput.push(" " + number);
     }
   }
   return triggerOutput;
