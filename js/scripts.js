6375f5aecea8c78d85c7a7369f90345e039e3975
3Emme <emmettbuentiempo@gmail.com>
2020-08-23 23:39:08 -0700

    remove console.logs from scripts.js to clean up code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Business Logic 
 function roboger (initialNumberString){ 
-  console.log("initialNumberString: " + initialNumberString);
   const numberArray =  countUp(initialNumberString);
 
   function countUp(endNumber){
@@ -12,8 +11,6 @@ function roboger (initialNumberString){
   }
 
   let arrayLength = parseInt(initialNumberString) + 1;
-  console.log("arrayLength: " + arrayLength);
-  console.log("numberArray: " + numberArray);
   return triggerDetect(arrayLength,numberArray);
 }
 
@@ -24,36 +21,23 @@ function triggerDetect(arrayLength,numberArray) {
   for (let number of numberArray){
     let numberLength = number.length;
     let numberDigitArray = number.split("");
-    console.log("numberDigitArray: " + numberDigitArray);
-    console.log("numberUnderScope: " + number);
-    console.log("Length of number under scope: " + numberLength);
-    console.log("numberArray index looped");
     let triggerDigit = 0;
       for (let digit of numberDigitArray){
-        console.log("digitArray digit looped");
         if (triggerList.includes(digit) && parseInt(digit) > triggerDigit) {
           triggerDigit = digit;
-          console.log("digit triggered");
         } else {
-        console.log("digit not triggered");
           }
       }
-    console.log("triggerDigit: " + triggerDigit);
     if (triggerDigit === "3"){
       triggerOutput.push("Won't you be my neighbor?");
-      console.log("triggerOutput: pushed #3");
     } else if (triggerDigit === "2"){
       triggerOutput.push("Boop!");
-      console.log("triggerOutput: pushed #2");
     } else if (triggerDigit === "1"){
       triggerOutput.push("Beep!");
-      console.log("triggerOutput: pushed #1");
     } else {
       triggerOutput.push(number);
-      console.log("no trigger, pushed number");
     }
   }
-    console.log("triggerOutput: " + triggerOutput);
   return triggerOutput;
 } 
 
